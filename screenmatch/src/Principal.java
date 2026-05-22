43d0b802f60081d2190cad5b8c4ae6bc4140bcd5
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-05-21 22:54:56 -0300

    Mesmo nome, comportamentos diferentes

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -7,6 +7,7 @@ public class Principal {
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setAnoDeLancamento(1978);
         meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -26,5 +27,7 @@ public class Principal {
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
     }
 }
